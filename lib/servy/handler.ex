b6c232d7bf66b9eac030cd9e8a2f4c862146dafe
Praveer <pravs.13@gmail.com>
2018-06-23 21:56:35 +0530

    Adding module docs

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,9 @@
 defmodule(Servy.Handler) do
+  @moduledoc "Handles HTTP Requests."
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc "Transforms the request into the response."
   def handle(request) do
     # conv = parse(request)
     # conv = route(conv)
@@ -8,8 +13,17 @@ defmodule(Servy.Handler) do
     |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
+
+  @doc "Logs 404 requests"
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts("Warning! #{path} is on the loose")
+    conv
+  end
+
+  def track(conv), do: conv
 
   def parse(request) do
     # TODO: Parse the request string into a map:
@@ -46,8 +60,27 @@ defmodule(Servy.Handler) do
     %{conv | status: 200, resp_body: "Bears #{id}"}
   end
 
+  def route(%{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   def format_response(conv) do
@@ -111,6 +144,18 @@ IO.puts(response)
 
 request = """
 GET /bears/1 HTTP/1.1
+HOST: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
 HOST: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
